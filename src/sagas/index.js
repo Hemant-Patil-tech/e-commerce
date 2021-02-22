ab219dd996e85ba64f811063c60bd1a9c5d9b751
Hemant Patil <hemantpatil9404@gmail.com>
2021-02-22 15:39:44 +0530

    axios call for fetchingjson

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,17 +1,24 @@
-import { put, takeLatest, all,takeEvery } from 'redux-saga/effects';
-import getInitialState from '../initialState'
+import { put, takeLatest,call, all,takeEvery } from 'redux-saga/effects';
+
+import axios from 'axios'
 
 
 function* fetchInitialState() {
-    console.log('fetchInitialState')
-    const json = yield  getInitialState();
-    console.log('InitialState', json)          
-    yield put({ type: "SET_INITIAL_STATE", json: json, });
+  const userData = yield call(getData);
+  console.log('',userData)
+  yield put({ type: "SET_INITIAL_STATE", json:userData.data }); 
   }
 
   function* actionWatcher() {
     yield takeEvery('GET_INITIAL_STATE', fetchInitialState)
 }
+
+
+function getData() {
+  return axios.get('initialState.json');
+};
+
+
 export default function* rootSaga() {
   yield all([
   actionWatcher(),
